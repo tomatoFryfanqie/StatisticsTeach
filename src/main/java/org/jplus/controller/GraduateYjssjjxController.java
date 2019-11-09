9b312c124cdd80d0c3ee76ce20f163582d9350d0
tomatoFryfanqie <1093369951@qq.com>
2019-11-09 12:48:49 +0800

    研究生整合(new db)

--- src/main/java/org/jplus/controller/GraduateYjssjjxController.java
+++ src/main/java/org/jplus/controller/GraduateYjssjjxController.java
@@ -2,10 +2,17 @@ package org.jplus.controller;
 
 import org.jplus.interceptor.NeedLogin;
 import org.jplus.pojo.Users;
+import org.jplus.pojo.yjssjjx.Yjssjjx;
+import org.jplus.pojo.yjssjjx.YjssjjxAccpet;
+import org.jplus.service.TjztService;
 import org.jplus.service.YjssjjxService;
+import org.jplus.utils.GetPracticeWorkLoad;
+import org.jplus.utils.GetYear;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +29,9 @@ public class GraduateYjssjjxController {
     @Autowired
     private YjssjjxService yjssjjxService;
 
+    @Autowired
+    private TjztService tjztService;
+
     @RequestMapping("/graduatepractice")
     @NeedLogin
     public String graduatepractice(Model model, Users users){
@@ -29,15 +39,36 @@ public class GraduateYjssjjxController {
         /*获取实践类型*/
         model.addAttribute("yjssjlx", yjssjjxService.getSjlx());
         /*获取研究生实践类型的基本信息*/
-        System.out.println(yjssjjxService.getYjssjjxJbxx(users.getGh()).toString());
-        model.addAttribute("yjssjjxJbxx", yjssjjxService.getYjssjjxJbxx(users.getGh()));
+        Yjssjjx jbxx = yjssjjxService.getYjssjjxJbxx(users.getGh());
+        if(jbxx!=null) {
+            /*获取指导学生次数工作量（显示）*/
+            model.addAttribute("zdxshdcsGzl", GetPracticeWorkLoad.getForYjsWorkLoad(yjssjjxService.getYjssjjxJbxx(users.getGh()).getZdxshdcs()));
+            /*获取自行增加的工作量*/
+            model.addAttribute("ZtGzl", jbxx.getGzl() - jbxx.getRcgzl() - GetPracticeWorkLoad.getForYjsWorkLoad(yjssjjxService.getYjssjjxJbxx(users.getGh()).getZdxshdcs()));
+        }
+        model.addAttribute("yjssjjxJbxx", jbxx);
         return "graduatepractice";
     }
 
     @NeedLogin
     @PostMapping("/updateYjssjjxInfo")
-    public String updateInfo(){
-
+    public String updateInfo(@ModelAttribute(value = "YjssjjxAccpet")YjssjjxAccpet yjssjjxAccpet,Users users){
+        /*先判断提交状态表的提交状态是否为0：未提交状态，为0则可以进行更新操作*/
+        if(tjztService.getTjzt(users.getGh()).getTjzt()==0){
+            /*如果数据库没有此工号的数据，则新增加一条数据*/
+            if(yjssjjxService.getYjssjjxJbxx(users.getGh())==null){
+                yjssjjxService.addYjssjjxInfo(users.getGh());
+            }
+            /*获取年度*/
+            yjssjjxAccpet.setNd(GetYear.getYears());
+            /*获取日常工作量*/
+            yjssjjxAccpet.setRcgzl(GetPracticeWorkLoad.getDayWorkLoad(yjssjjxAccpet.getSjts1(),yjssjjxAccpet.getSjts2(),yjssjjxAccpet.getSjts3()));
+            /*获取总工作量,1.日常工作量，2.指导工作量,3 自输入工作量*/
+            yjssjjxAccpet.setGzl(yjssjjxAccpet.getRcgzl()+GetPracticeWorkLoad.getForYjsWorkLoad(yjssjjxAccpet.getZdxshdcs())+yjssjjxAccpet.getGzl());
+            /*更新信息*/
+            yjssjjxAccpet.setGh(users.getGh());
+            yjssjjxService.updateYjsSxJbxx(yjssjjxAccpet);
+        }
         return "redirect:graduatepractice";
     }
 }
